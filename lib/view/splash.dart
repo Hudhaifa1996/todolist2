a054ce62654a2eee488f80fedc5e8ffe4349f9cd
Hudhaifa2 <hozifa.alobidy@gmail.com>
2024-10-09 14:29:18 +0300

    lookup fail

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -5,30 +5,86 @@ import 'package:go_router/go_router.dart';
 import 'package:todolists/routing.dart';
 import 'package:todolists/viewmodel/vm_home.dart';
 import 'package:flutter/scheduler.dart';
+//
+// class SplashScreen extends StatelessWidget {
+//    SplashScreen({super.key});
+//
+//    final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+// bool isLoading = true;
+//   @override
+// void didChangeDependencies (){
+//     if (isLoading) {VmHome(context).fetchData(context);
+//     isLoading = false;
+//     }
+//     super.didChangeDependencies ()
+//   };
+//    Widget build(BuildContext context) {
+//     return Scaffold(
+//       // key: myGlobals.scaffoldKey,
+//       key: _scaffoldKey,
+//
+//       body: FutureBuilder(
+//           future: Future.wait ([getNextRoute(Routing.prefs),  VmHome(context).fetchData(context)]),
+//           initialData: 'wait',
+//           builder: ( context2, AsyncSnapshot<dynamic> snapshot) {
+//             if (snapshot.connectionState == ConnectionState.done) {
+//               WidgetsBinding.instance.addPostFrameCallback((_) {
+//                 // print(snapshot);
+//                 // Navigator.of(context2).pop();
+//                 if (context2.mounted) {Future.delayed(Duration.zero).then ((value){context2.go('${snapshot.data[0]}');});}
+//                 // Future.delayed(Duration.zero).then ((value){}); //(const Duration(milliseconds: 300));
+//                 // GoRouter.of(context).go('${snapshot.data[0]}');
+//                 // GoRouter.of(context)
+//
+//                 // context.go('${snapshot.data[0]}');
+//               });
+//             }
+//             return const Center(
+//                 child: SpinKitPouringHourGlassRefined(
+//               color: Colors.redAccent,
+//               size: 300.0,
+//             ));
+//           }),
+//     );
+//   }
+// }
 
-class SplashScreen extends StatelessWidget {
+
+class SplashScreen extends StatefulWidget {
    SplashScreen({super.key});
 
-   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool isLoading = true;
+  @override
+  void didChangeDependencies (){
+    if (isLoading) {VmHome(context).fetchData(context);
+    isLoading = false;
+    }
+    super.didChangeDependencies ();
+  }
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       // key: myGlobals.scaffoldKey,
       key: _scaffoldKey,
 
       body: FutureBuilder(
-          future: Future.wait ([getNextRoute(Routing.prefs), VmHome(context).fetchData(context)]),
+          future: getNextRoute(Routing.prefs),
           initialData: 'wait',
-          builder: ( context, AsyncSnapshot<dynamic> snapshot) {
+          builder: ( context2, AsyncSnapshot<dynamic> snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               WidgetsBinding.instance.addPostFrameCallback((_) {
-                // print(snapshot);
+                print(snapshot);
                 // Navigator.of(context2).pop();
-                Future.delayed(const Duration(milliseconds: 300));
-                GoRouter.of(context).go('${snapshot.data[0]}');
-
+                // context.go('${snapshot.data}');
+                if (context.mounted) {context.go('${snapshot.data[0]}');}
+                // Future.delayed(Duration.zero).then ((value){}); //(const Duration(milliseconds: 300));
+                // GoRouter.of(context).go('${snapshot.data[0]}');
                 // GoRouter.of(context)
 
                 // context.go('${snapshot.data[0]}');
@@ -36,10 +92,10 @@ class SplashScreen extends StatelessWidget {
             }
             return const Center(
                 child: SpinKitPouringHourGlassRefined(
-              color: Colors.redAccent,
-              size: 300.0,
-            ));
+                  color: Colors.redAccent,
+                  size: 300.0,
+                ));
           }),
-    );
+    );;
   }
 }
